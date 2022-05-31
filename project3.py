b093eec5992ecc9da0ef9d6899798e11fb83847f
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-05-31 11:59:15 +0900

    Update and rename project1.py to project3.py

--- project3.py
+++ project3.py
@@ -29,11 +29,3 @@ service_price()
 부가세를 포함합니까? y/n: n
 67만 원입니다
 
-for item in range(2, 20):
-  for each in range(2, 20):
-    print(item,' X ', each, ' = ', item * each)
-   
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for item in nuber:
-  print(2 * item)
-  
