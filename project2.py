a1b78a1eb51aef1b3bbff7e88330a7c930c1a1e2
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-05-31 12:01:08 +0900

    Create project2.py

---
+++ project2.py
@@ -0,0 +1,8 @@
+for item in range(2, 20):
+  for each in range(2, 20):
+    print(item,' X ', each, ' = ', item * each)
+   
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for item in nuber:
+  print(2 * item)
+  
