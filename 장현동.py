19fb7f74a703cf842c85b4bef0d746345aae1bc2
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-05-31 11:58:03 +0900

    Create 장현동.py

---
+++ 장현동.py
@@ -0,0 +1,39 @@
+price = [23, 40, 67]
+
+for i in price:
+    i * 1.1
+
+    def service_price():
+    service = input('서비스 종류를 입력하세요, a/b/c: ')
+    if valueAdded == 'y':
+        if service == 'a':
+            result = 23 * 1.1
+        if service == 'b':
+            result = 40 * 1.1
+        if service == 'c':
+            result = 67 * 1.1
+        if valueAdded == 'n':
+            if service == 'a':
+                result == 23
+        if service == 'c':
+            result = 67
+        print(round(result, 1), '만 원입니다')
+        service_price()
+
+        
+서비스 종류를 입력하세요, a/b/c: b
+부가세를 포함합니다? y/n: y
+44.0만 원입니다
+service_price()
+서비스 종류를 입력하세요, a/b/c: c
+부가세를 포함합니까? y/n: n
+67만 원입니다
+
+for item in range(2, 20):
+  for each in range(2, 20):
+    print(item,' X ', each, ' = ', item * each)
+   
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for item in nuber:
+  print(2 * item)
+  
