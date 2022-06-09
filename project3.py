39d26f22577ccdd26654bef3b8c7f1e01e960b49
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-06-09 14:52:20 +0900

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,3 +1,10 @@
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    if i %2 is not 0:
+        print(i, '홀수')
+    else:
+        print(i, '짝수')
+
+
 price = [23, 40, 67]
 for i in price:
     print(i * 1.1)
