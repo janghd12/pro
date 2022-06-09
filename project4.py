3b0f6f0ff8878be9f77f4f03a1e4b663cac146be
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-06-09 15:19:42 +0900

    Update project4.py

--- project4.py
+++ project4.py
@@ -1,26 +1,34 @@
 import os, re
-os.chdir(r'C:\Users\user\python')
-f = open('friends101.txt', 'r', encoding = 'utf8')
+os.chdir("C:\\Users\\user\\Downloads")
+f = open("friends101.txt", "r", encoding = "utf-8")
 script101 = f.read()
-print(script101[:100]
+print(script101[:100])
 
 Line = re.findall(r'Monica:.+', script101)
 print(Line[:3])
-      
+
 for item in Line[:3]:
-print(item)
+    print(item)
+
 f.close()
-f = open('monica.txt', 'w', encoding = 'utf8')
-monica = ''
+
+f = open('monica.txt', "w", encoding = "utf8")
+
+monica = ""
 for i in Line:
-monica += i 
-f.weite(monica)
-f.close()
-print('다음과 같은 표기를 줄을 바꿀 수 있습니다. \.n이제 줄이 바뀌었군요!')
-monica = ''
-for i in Line:
-monica += i +'\n'
-monica[:100]
-f = open('monica.txt', 'w', encoding = 'utf-8')
+    monica += i 
+
 f.write(monica)
 f.close()
+
+print('다음과 같은 표기를 줄을 바꿀 수 있습니다. \n이제 줄이 바뀌었군요!')
+monica = ""
+for i in Line:
+    monica += i + "\n"
+    
+monica[:100]
+
+f = open("monica.txt", 'w', encoding = "utf-8")
+f.write(monica)
+f.close()
+
