b9cb215614d0c8859d7668071281b48871632156
janghd12 <84713665+janghd12@users.noreply.github.com>
2022-06-09 15:30:44 +0900

    Update project5.py

--- project5.py
+++ project5.py
@@ -7,40 +7,40 @@ set(a)
 
 set(re.findall(char, script101))
 
-rachel = "Rachel:'
-rachel = re.sub(':', '', rachel)
+rachel = "Rachel:"
+rachel = re.sub(":", '', rachel)
 rachel
 
-rache[:-1]
+rachel[:-1]
 
-y - set(re.findall(char,script101))
+y = set(re.findall(char,script101))
 z = list(y)
 character = []
 for i in z:
-  character += [i[:-1]]
+    character += [i[:-1]]
   
 character
 
+character = [x[:-1] for x in list(set(re.findall(r'[A-Z][a-z]+:',script101)))]
 character
- = [X[:-1] for x in list(set(re.findall(r'[A-Z][a-z]+:',script101)))]
-  character
 
-  re.findall(r'\([A-Za-z].+[a-z|\.]\)', script101)[:6]
+re.findall(r'\([A-Za-z].+[a-z|\.]\)', script101)[:6]
   
-  import os, re
-  os.chdir(r'C:\Users\user\python')
-  f = open('friends101.txt', 'r')
-  f.read(100)
-  f.seek(0)
+import os, re
+os.chdir(r'C:\\Users\\user\\Downloads')
+f = open('friends101.txt', 'r')
+f.read(100)
+
+f.seek(0)
   
-  sentences = f.readlines()
-  sentences[:3]
+sentences = f.readlines()
+sentences[:3]
   
-  for i in sentences[:20]:
+for i in sentences[:20]:
     if re.match(r'[A-Z][a-z]+:',i):
-      print(i)
-     
- lines = [i for i in sentences if re.match(r'[A-z][a-z]+:', i)]
+        print(i)
+         
+lines = [i for i in sentences if re.match(r'[A-z][a-z]+:', i)]
 lines[:10]
 
 would = [i for i in sentences if re.match(r'[A-z][a-z]+:', i) and re.search('would', i)]
@@ -50,7 +50,7 @@ take =  [i for i in sentences if re.match(r'[A-z][a-z]+:', i) and re.search('tak
 take
 
 for i in take:
-  print(i)
+    print(i)
   
 newf = open('would.txt', 'w')
 newf.writelines(would)
